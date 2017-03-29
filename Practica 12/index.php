9db3a6a03c1777c4b28c693f62f3c5a0ee04a2c3
Rosa Elvia Cruz Vazquz <rosaecv.96@gmail.com>
2017-03-29 09:48:51 -0600

    Add files via upload

---
+++ Practica 12/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+     <head>
+	      <title>PRACTICA 12- Formularios</title>
+		  </head>
+		  <body>
+		       <div >
+			        <form name="formulario" method="post" action="procesar.php">
+					Nombre: <input type="text" name="nombre" value="">
+					Apellidos: <input type="text" name="nombre" value="">
+					<input type="submit" value="Enviar"/>
+					</form>
+					</div>
+					<a href="area.php">Calcular area del circulo</a>
+					</body>
+				    </html>
